5ffc9e2cafac3ce553bbb0e0dbc2f0651ef202e9
Ricardo Sanchez <ricardo@ontechies.com>
2017-11-05 01:19:12 -0600

    Updated the response from the function

--- whattobring/index.js
+++ whattobring/index.js
@@ -12,25 +12,24 @@ module.exports = function (context, req) {
 
         switch(city.toLowerCase()) {
             case "austin":
-                result = "Clothing: Jeans, shorts, t-shirts. Shoes: Boots, sandals, and sneakers. Accessories: Sunglasses, sunscreen"
+                result = city + ": " + "Clothing: Jeans, shorts, t-shirts. Shoes: Boots, sandals, and sneakers. Accessories: Sunglasses, sunscreen"
                 break;
             case "san francisco":
-                result = "Clothing: Jeans, t-shirts, warm windproof layer. Shoes: Comfortable shoes, sneakers. Accessories: Sunglasses, sunscreen"
+                result = city + ": " + "Clothing: Jeans, t-shirts, warm windproof layer. Shoes: Comfortable shoes, sneakers. Accessories: Sunglasses, sunscreen"
                 break;
             case "los angeles":
-                result = "Clothing: Jeans, t-shirts, sweatshirt, light jacket. Shoes: Flat shoes, sandals, sneakers. Accessories: Long scarves, hat"
+                result = city + ": " + "Clothing: Jeans, t-shirts, sweatshirt, light jacket. Shoes: Flat shoes, sandals, sneakers. Accessories: Long scarves, hat"
                 break;
             case "new york":
-                result = "Clothing: Jeans, t-shirts, shirts, sweater. Shoes: Comfortable shoes, sneakers. Accessories: Scarf, beanie hat, gloves"
+                result = city + ": " + "Clothing: Jeans, t-shirts, shirts, sweater. Shoes: Comfortable shoes, sneakers. Accessories: Scarf, beanie hat, gloves"
                 break;
             default:
-                result = "We dont't have any data for that city."
+                result = "We dont't have any data for " + city
         }
 
         context.res = {
             // status: 200, /* Defaults to 200 */
-            
-            body: "Hey there, here are the things I suggest you bring with you to " + city + "\n" + result
+            body: result
         };
     }
     else {
